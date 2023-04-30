c8445fec2be1751afc3638b760c749f02bf34a34
bladyzamosc <emil.klimek@consultant.volvo.com>
2023-04-30 18:24:52 +0200

    create file and randomize

--- draw_records.py
+++ draw_records.py
@@ -1,15 +1,26 @@
 import os
+import shutil
+from datetime import datetime
 
 from randomizer import Randomizer
 
-files = [f for f in os.listdir(os.path.join(os.curdir, "files")) if not f.startswith("~")]
+# datetime object containing current date and time
+now = datetime.now()
+print("now =", now)
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d%m%Y_%H%M%S")
+print("date and time =", dt_string)
+
+files = [f for f in os.listdir(os.path.join(os.curdir, "files")) if not f.startswith("~") and f.endswith("xlsx")]
 print(files)
 
+from_path = os.path.join(os.curdir, "files")
+to_path = os.path.join(os.curdir, "files", dt_string);
+os.makedirs(to_path, exist_ok=True, mode=777)
+
 for f in files:
-    r = Randomizer(f, os.path.join(os.curdir, "files"))
+    file = os.path.join(from_path, f)
+    to_file = os.path.join(to_path, f)
+    shutil.copyfile(file, to_file)
+    r = Randomizer(to_file)
     r.randomize()
-
-
-
-
-
--- randomizer.py
+++ randomizer.py
@@ -1,4 +1,4 @@
-import os
+import random
 
 import openpyxl
 
@@ -12,38 +12,54 @@ MAX_VALUES = 1000
 
 CELL_H = 'H'
 
+CELL_M = 'M'
+
+
+def extract_values(ws):
+    result = {}
+    for cell in ws[CELL_H]:
+        value = cell.value
+        if (isinstance(value, int) or isinstance(value, float)) and value is not None:
+            result[cell.row] = value
+        if cell.row > MAX_VALUES:
+            print("Koniec")
+            break
+    return result
+
 
 class Randomizer:
-    def __init__(self, file, path):
+    def __init__(self, file):
         self.file = file
-        self.path = path
 
     def randomize(self):
-        ws = self.active_sheet()
-        values = self.extract_values(ws)
-        top_number = TOP_3_PERCENT if TOP_3_PERCENT > int(len(values) * TOP_3_PERCENT / ONE_HUNDRED) else int(
-            len(values) * TOP_3_PERCENT / ONE_HUNDRED)
-        rest_number = REST_2_PERCENT if REST_2_PERCENT > int(len(values) * REST_2_PERCENT / ONE_HUNDRED) else int(
-            len(values) * REST_2_PERCENT / ONE_HUNDRED)
-
-        print(values, top_number, rest_number)
-
-    def active_sheet(self):
         wb = self.open_ws()
         ws = wb.active
-        return ws
+        vals = extract_values(ws)
+        top3 = round(len(vals) * TOP_3_PERCENT / ONE_HUNDRED)
+        top_number = TOP_3_PERCENT if TOP_3_PERCENT > top3 else top3
+        rest2 = round(len(vals) * REST_2_PERCENT / ONE_HUNDRED)
+        rest_number = REST_2_PERCENT if REST_2_PERCENT > rest2 else rest2
+        sorted_values = sorted(vals.items(), key=lambda x: x[1], reverse=True)
+        top_elements = sorted_values[0:top_number]
+        rest_elements = sorted_values[top3 - 1:len(sorted_values)]
+        random.shuffle(rest_elements)
+        rest_elements = rest_elements[0:rest_number]
+        draw = wb.create_sheet("Losowanie")
+        mc = ws.max_column
+
+        row_new = 1
+        for t in top_elements:
+            for j in range(1, mc + 1):
+                cell_from = ws.cell(row=t[0], column=j)
+                draw.cell(row=row_new, column=j).value = cell_from.value
+            row_new = row_new + 1
+        for t in rest_elements:
+            for j in range(1, mc + 1):
+                cell_from = ws.cell(row=t[0], column=j)
+                draw.cell(row=row_new, column=j).value = cell_from.value
+            row_new = row_new + 1
+        wb.save(self.file)
 
     def open_ws(self):
-        fullname = os.path.join(self.path, self.file)
-        wb = openpyxl.load_workbook(fullname)
+        wb = openpyxl.load_workbook(self.file, data_only=True)
         return wb
-
-    def extract_values(self, ws):
-        result = {}
-        for cell in ws[CELL_H]:
-            value = cell.value
-            if isinstance(value, int) and value is not None:
-                result[cell.row] = value
-            if cell.row > MAX_VALUES:
-                break
-        return result
